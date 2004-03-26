f8334bbe5189c830232558bc1d345bca590d6184
rpk <rpk@f4139a73-77d6-0310-a459-e303945e5726>
2004-03-26 16:10:23 +0000

    updates

--- client.py
+++ client.py
@@ -1,24 +1,85 @@
 #!/usr/bin/env python
-#Subetha edit paper here: http://squeak.local./~twl/subethaedit-paper.pdf
-#SubPythonEdit Client Piece
-# Jean-Paul "exarkun" Calderone
-# Allen "dash" Short
+# Subetha edit paper here: http://squeak.local./~twl/subethaedit-paper.pdf
+# SubPythonEdit Client and Server Piece
+#  Jean-Paul "exarkun" Calderone
+#  Allen "dash" Short
+#  Abhay "ark3" Saxena
 
 import sys, socket
-from twisted.spread import pb
 from optparse import OptionParser
+
+from twisted.spread import pb
+import twisted.spread.newjelly
+
+# cocoa imports go here, before the reactor is imported.
+"""
+from twisted.internet import cfsupport
+cfsupport.install(top-level-object)
+"""
+
+from twisted.internet import reactor
+from twisted.python import log
+
 import events
+
+# XXX ought to pick appropriate name for log file based on
+# server/client status and client unique ID
+#log.startLogging(open("sub.log", 'w')) 
+log.startLogging(sys.stdout)
 
 # nasty global thingy
 spe_port = 8910
 spe_hostname = ''
 spe_server = 'client'
 
-# cocoa imports go here.
-from twisted.internet import reactor
+# ------------------------------------------------------------------
+# Server Code
+# ------------------------------------------------------------------
 
-from twisted.python import log
-#log.startLogging(open("sub.log", 'w'))
+# http://twistedmatrix.com/documents/howto/pb-copyable
+class EditorRoot(pb.Root):
+    #__implements__ = (pb.IPBRoot,)
+
+    def __init__(self):
+        self.notificationList = []
+        self.version = 0.1
+
+    def remote_event(self, event):
+        """
+        Receive client events and process according to event class
+        Event Types:  insert/delete, cursor/select
+            Sync is a large 'insert' -- see readme for idea caching
+        """
+        print 'Event received', event
+        dead = []
+        for client in self.notificationList:
+            try:
+                client.callRemote("event", event)
+                # insert into pingList dictionary timestamp of last contact
+                
+            except pb.DeadReferenceError:
+                dead.append(client)
+        map(self.notificationList.remove, dead)
+
+    def remote_attach(self, client):
+        """
+        Receive attach event from twisted - insert client into broadcast list
+        """
+        print 'Client attached', client
+        self.notificationList.append(client)
+        # add client name/id into ping list to track timed-out clients
+
+    def remote_detach(self, client):
+        """
+        Receive detach event from twisted - remove client from the broadcast list
+        """
+        print 'Client detached', client
+        self.notificationList.remove(client)
+        # remove client name/id from ping list
+
+# ------------------------------------------------------------------
+# Client Code
+# ------------------------------------------------------------------
 
 class Client(pb.Referenceable):
     def remote_event(self, event):
@@ -29,7 +90,7 @@ class Client(pb.Referenceable):
 #            self.handleEditEvent(Event(replacementString, randomColor()))
         
 
-from twisted.internet import stdio
+from twisted.internet import stdio  # This is going away, right?
 
 def cbGotRootObject(root):
     print 'Got root object', root
@@ -68,33 +129,43 @@ class UserInputProtocol(basic.LineReceiver):
 #  (other types Referenceable, Cacheable)
 #
 
-"""
-from twisted.internet import cfsupport
-cfsupport.install(top-level-object)
-"""
+# ------------------------------------------------------------------
+# Main/Startup code
+# ------------------------------------------------------------------
 
 # refactor: use twisted.Application (launch via twistd)
-def main():
-    parser = OptionParser()
-    parser.add_option('-s','--server', dest='spe_server', default=False,
-                        help='configure me to be a server')
-    parser.add_option('-n', '--hostname', dest='spe_hostname', default='',
-                        help='Hostname of server')
-    parser.add_option('-p', '--port', dest='spe_port', default=8910,
-                        help='subpythonedit server port')
-    
-    (options, args) = parser.parse_args()
-    if len(sys.argv) > 1:
-        hostname = sys.argv[1]
-    else:
-        hostname = "127.0.0.1"
+def server_main(port):
+    f = pb.PBServerFactory(EditorRoot())
+    p = reactor.listenTCP(port, f)
+    reactor.run()
 
+def client_main(hostname, port):
     f = pb.PBClientFactory()
     rootDeferred = f.getRootObject()
     rootDeferred.addCallback(cbGotRootObject)
     rootDeferred.addErrback(ebGotRootObject)
-    c = reactor.connectTCP(hostname, 8910, f)
+    c = reactor.connectTCP(hostname, port, f)
     reactor.run()
+
+# refactor: use twisted.Application (launch via twistd)
+def main():
+    parser = OptionParser()
+    parser.add_option('-s','--server', action='store_true', dest='isserver',
+                           default=False, help='configure me to be a server')
+    parser.add_option('-n', '--hostname', dest='hostname', default='127.0.0.1',
+                        help='Hostname of server')
+    parser.add_option('-p', '--port', dest='port', default=8910,
+                        help='subpythonedit server port')
+    
+    (options, args) = parser.parse_args()
+
+    print options
+    if options.isserver:
+        # run as server
+        # ...
+        server_main(options.port)
+
+    client_main(options.hostname, options.port)
 
 if __name__ == '__main__':
     main()
--- readme.txt
+++ readme.txt
@@ -1,19 +1,31 @@
 Contributors:
 
- Ian Jones ianjones@umich.edu
- Bob Kuehne rpk@blue-newt.com
- Mike "bear" Taylor bear@code-bear.com
-
+ Ian Jones, ianjones@umich.edu
+ Bob Kuehne, rpk@blue-newt.com, aim: rpk@mac.com
+ Abhay Saxena, ark3@email.com
+ Mike "bear" Taylor, bear@code-bear.com, aim: codebear42, msn: bear@code-bear.com
+ Nick Bastin, nbastin@mac.com, aim: mondragon0
+ Jean-Paul "exarkun" Calderone @twisted.com
+ Allen "dash" Short @twisted.com
 
 
 """
  features
   - authentication
+    - subetha-style approve/deny on per-person basis
+    - password control access to doc
+    - batch approval, rejection
+    - global notification of pending users
   - client undo open issue. undo is weird.
+      (bear: something tells me undo will be server side only and passed to all
+             - yeah, this bears (pi) further debate)
   - later, non-text edits. have events that support payloads of
     things other than text blocks. and other doc types. for
     example, payloads of images, formatted text, etc.
   - rehost server
+  - Better local save support - no requirement for continual Save Copy As...
+  - Also shared save support, I suppose - like on LAN NFS - everybody saves to the same point
+      (some way of passing a uri or webdav or ???)
 
  notes
    - text sequences are a logical unit. two kinds:
